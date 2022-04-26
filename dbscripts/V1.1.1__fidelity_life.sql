372362d8d8b7f6f994798d391c601e28aa2cc043
payal <payal.jindal@complereinfosystem.com>
2022-04-26 15:49:30 +0530

    updates in sql

--- dbscripts/V1.1.1__fidelity_life.sql
+++ dbscripts/V1.1.1__fidelity_life.sql
@@ -8,8 +8,6 @@ create table fidelity_test_users if not exists(
 );
 
 
-
-
 create table fidelity_users if not exists(
   name varchar (100),  -- variable string column
   preferences string, -- column used to store JSON type of data
