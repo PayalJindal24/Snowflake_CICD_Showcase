f3db119b65fece28e03b3b6fd01955a29e52492b
payal <payal.jindal@complereinfosystem.com>
2022-04-26 14:13:11 +0530

    sql uodate

--- dbscripts/V1.1.1__fidelity_life.sql
+++ dbscripts/V1.1.1__fidelity_life.sql
@@ -1,6 +1,6 @@
-use database demo_db;
-use schema schemachange;
-Select 'abc';
+use database SNOWFLAKE_CICD_SHOWCASE;
+use schema PUBLIC;
+
 create table fidelity_test_users (
   name varchar (100),  -- variable string column
   preferences string, -- column used to store JSON type of data
